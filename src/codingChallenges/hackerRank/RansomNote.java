b241d3c75cf82baed19a78a543f1a0e8dda11021
Tim Johnston <tedwardj11@gmail.com>
2018-12-23 21:54:09 -0500

    implemented RansomNote but solution not efficient enough - needs improvement

--- src/codingChallenges/hackerRank/RansomNote.java
+++ src/codingChallenges/hackerRank/RansomNote.java
@@ -5,28 +5,54 @@ import java.util.Map;
 
 public class RansomNote {
 
-	public void ransomeNote() {
+	public void ransomNote() {
 	
-		String[] magazine = {"give", "me", "one", "grand", "tonight"};
-		String[] note = {"give", "one", "grand", "tonight"};
+		String[] magazine = {"two", "times", "three", "is", "not", "four"};
+		String[] note = {"two", "times", "two", "is", "four"};
 		
-		ransomeNoteImpl(magazine, note);
+		ransomNoteImpl(magazine, note);
 	
 	}
 
-	private void ransomeNoteImpl(String[] magazine, String[] note) {
+	private void ransomNoteImpl(String[] magazine, String[] note) {
 		
-		Map<Integer, String> noteMap = new HashMap<Integer, String>();
-		Map<Integer, String> magazineMap = new HashMap<Integer, String>();
+		boolean success = true;
+		Map<String, Integer> noteMap = new HashMap<String, Integer>();
+		Map<String, Integer> magazineMap = new HashMap<String, Integer>();
 		
+		//Make note map
 		for (int i = 0; i < note.length; i++) {
-			noteMap.put(i, note[i]);
+			
+			if (noteMap.containsKey(note[i])) {
+				int previousCount = noteMap.get(note[i]);
+				noteMap.put(note[i], previousCount + 1);
+			} else {
+				noteMap.put(note[i], 1);
+			}
+			
 		}
 		
+		//Make magazine map
 		for (int i = 0; i < magazine.length; i++) {
-			magazineMap.put(i, note[i]);
+			if (magazineMap.containsKey(magazine[i])) {
+				magazineMap.put(magazine[i], magazineMap.get(magazine[i] + 1));
+			} else {
+				magazineMap.put(magazine[i], 1);
+			}
 		}
 		
+		//Check for occurrence of note words in magazine map
+		for (String word : noteMap.keySet()) {
+			
+			if (!(magazineMap.containsKey(word) && magazineMap.get(word) >= noteMap.get(word))) {
+				success = false;
+				break;
+			}
+			
+		}
+		
+		
+		System.out.println(success ? "Yes" : "No");
 		
 	}
 	
